7e79af678cac749d8fb7791190e506d4b40590ea
Landon <mccracken.landon@gmail.com>
2021-01-06 18:54:26 -0500

    i should commit more

--- yaravent.py
+++ yaravent.py
@@ -2,16 +2,21 @@ import yara
 import os
 import string
 import sys
+import argparse
 from multiprocessing import Process
 import Evtx.Evtx as evtx
 
 class Args():
-    def __init__(self):
-        self.threads = 4
-        self.depth = 2
-        self.rulesDir = "./yara"
-        self.logsDir = "./logs"
-        self.resultsDir = "./"
+    def __init__(self, parsed):
+        self.depth = parsed.depth
+        self.rulesDir = parsed.yara
+        self.logsDir = parsed.logs
+        self.resultsDir = parsed.results
+        self.write_matches = parsed.misses
+    def __str__(self):
+        return(f"depth = {self.depth}, yara = {self.rulesDir}, logs = {self.logsDir}, results = {self.resultsDir}, missed = {self.write_matches}")
+    def __repr__(self):
+        return self.__str__()
 
 
 class Rules():
@@ -38,11 +43,11 @@ class Rules():
             for f in fn:
                 if f.endswith(".yara") or f.endswith(".yar"):
                     self.files.append(os.path.join(dp, f))
-        if self.depth < self.maxDepth:
-            for d in dn:
-                self._get_rules(d)
-        else:
-            return
+            if self.depth < self.maxDepth:
+                for d in dn:
+                    self._get_rules(d)
+            else:
+                return
 
 class Logs():
     def __init__(self, args):
@@ -56,41 +61,47 @@ class Logs():
                 if f.endswith('.evtx'):
                     self.logs.append(os.path.join(dp, f))
 
-class Scan():
-    def __init__(self, rules, log, args):
-        self.rules = rules
-        self.log = log
-        self.resultDir = args.resultsDir
+def scan(log, rules, args):
+    log_name = os.path.basename(log)
+    hits = os.path.join(args.resultsDir, log_name + ".hits")
+    misses = os.path.join(args.resultsDir, log_name + ".misses")
 
-    def scan(self):
-        with evtx.Evtx(self.log) as log:
-            for record in log.records():
+    hitsFn = open(hits, "a")
+    missesFn = open(misses, "a")
+
+    with evtx.Evtx(log) as curlog:
+            for record in curlog.records():
                 xmlRecord = record.xml()
-                for rule in self.rules.rules:
+                for rule in rules:
                     matches = rule.match(data=xmlRecord)
                     if matches:
-                        print(matches)
+                        for match in matches:
+                            hitsFn.write(xmlRecord)
+                    if matches and args.write_matches:
+                        missesFn.write(xmlRecord)
 
-        
+
 def main():
-    args = Args()
-    
+
+    parser = argparse.ArgumentParser(description="Scans Event logs agains yara rules")
+    parser.add_argument('-y', '--yara', metavar='path', type=ascii, required=True, help="Directory of the yara rules")
+    parser.add_argument('-l', '--logs', metavar='path', type=ascii, required=True, help="Directory containing the evtx files")
+    parser.add_argument('-r', '--results', metavar='path', type=ascii, required=True, help="Directory to store the results")
+    parser.add_argument('-d', '--depth', metavar='N', type=int, default=2, help="Recussion depth when looking for scans")
+    parser.add_argument('-m', '--misses', action='store_true', help="Will write a misses file containing all missed events")
+    parsed = parser.parse_args()
+    args = Args(parsed)
+    print(args)
     rules = Rules(args)
     rules.parseRules()
 
     logs = Logs(args)
     logs.parseEventFiles()
-    processes = []
+
     for log in logs.logs:
-        scan = Scan(rules, log, args)
-        p = Process(target=scan.scan())
-        processes.append(p)
+        p = Process(target=scan, args=(log, rules.rules, args))
         p.start()
 
-    for process in processes:
-        process.join()
-    #scan with yara rules
-    #write results
 
 if __name__ == "__main__":
     main()
