647ad3b7d717a5730f3799a8be8b5260f950e655
Landon <mccracken.landon@gmail.com>
2021-01-07 16:56:51 -0500

    loads of changes

--- yaravent.py
+++ yaravent.py
@@ -3,6 +3,7 @@ import os
 import string
 import sys
 import argparse
+from dicttoxml import dicttoxml
 from multiprocessing import Process
 import Evtx.Evtx as evtx
 from Evtx.Views import evtx_chunk_xml_view
@@ -14,6 +15,12 @@ class Args():
         self.logsDir = parsed.logs
         self.resultsDir = parsed.results
         self.write_misses = parsed.misses
+        self.recurs = parsed.recursive
+        try:
+            self.maxLog = parsed.max
+        except:
+            self.maxLog = None
+        self.force = parsed.force
     def __str__(self):
         return(f"depth = {self.depth}, yara = {self.rulesDir}, logs = {self.logsDir}, results = {self.resultsDir}, missed = {self.write_misses}")
     def __repr__(self):
@@ -23,8 +30,7 @@ class Args():
 class Rules():
     def __init__(self, args):
         self.root = args.rulesDir
-        self.maxDepth = args.depth
-        self.depth = 0
+        self.args = args
         self.rules = []
         self.files = []
     
@@ -33,57 +39,72 @@ class Rules():
         for f in self.files:
             try:
                 self.rules.append(yara.compile(filepath=f))
-            except:
-                #TODO: Log error
+            except Exception as e:
+                print(f"Problem loading yara file = {f}", file=sys.stderr)
+                print(e, file=sys.stderr)
                 pass
 
     def _get_rules(self, root):
-        # TODO handle externals in yara
-        self.depth += 1
-        for dp, dn, fn in os.walk(self.root):
+        for dp, _, fn in os.walk(self.root):
             for f in fn:
                 if f.endswith(".yara") or f.endswith(".yar"):
                     self.files.append(os.path.join(dp, f))
-            if self.depth < self.maxDepth:
-                for d in dn:
-                    self._get_rules(d)
-            else:
-                return
+            if not self.args.recurs:
+                break
 
 class Logs():
     def __init__(self, args):
         self.root = args.logsDir
+        self.args = args
         self.logs = []
     
-    def parseEventFiles(self):
-        # TODO test if already scanned?
+    def parseLogFiles(self):
+        skip = []
+        if not self.args.force:
+            for f in os.listdir(self.args.resultsDir):
+                f = f.replace(".misses", "").replace(".hits", "")
+                skip.append(f)
         for dp, _, fn in os.walk(self.root):
             for f in fn:
-                if f.endswith('.evtx'):
+                if len(self.logs) >= self.args.maxLog:
+                    break
+                if f.endswith('.evtx') and f not in skip:
                     self.logs.append(os.path.join(dp, f))
+            break
+
+def write_hit(f, match, xmlRecord):
+    f.write("<match>")
+    f.write("<rule>")
+    f.write(dicttoxml(match, attr_type=False, root=False).decode("utf-8"))
+    f.write("</rule>")
+    f.write(xmlRecord)
+    f.write("</match>")
+
+def write_misses(f, xmlRecord):
+    f.write("<miss>")
+    f.write(xmlRecord)
+    f.write("</miss>")
 
 def scan(log, rules, args):
     log_name = os.path.basename(log)
     hits = os.path.join(args.resultsDir, log_name + ".hits")
     misses = os.path.join(args.resultsDir, log_name + ".misses")
 
-    hitsFn = open(hits, "a")
+    hitsFn = open(hits, "w")
+    hitsFn.write('<?xml version="1.0"?>')
     if args.write_misses:
-        missesFn = open(misses, "a")
+        missesFn = open(misses, "w")
+        missesFn.write('<?xml version="1.0"?>')
     
     with evtx.Evtx(log) as curlog:
-            for chunk in curlog.chunks():
-                xmlRecords = []
-                for record in chunk.records():
-                    xmlRecords.append(record.xml())
-                xmlRecord = ''.join(xmlRecords)
-                for rule in rules:
-                    matches = rule.match(data=xmlRecord)
-                    if matches:
-                        for match in matches:
-                            hitsFn.write(xmlRecord)
-                    if matches and args.write_misses:
-                        missesFn.write(xmlRecord)
+                for record in curlog.records():
+                    xmlRecord = record.xml()
+                    for rule in rules:
+                        matches = rule.match(data=xmlRecord)
+                        if matches:
+                            write_hit(hitsFn, matches, xmlRecord)
+                        if matches and args.write_misses:
+                            missesFn.write(xmlRecord)
     hitsFn.close()
     if args.write_misses:
         missesFn.close()
@@ -96,7 +117,10 @@ def main():
     parser.add_argument('-l', '--logs', metavar='path', required=True, help="Directory containing the evtx files")
     parser.add_argument('-r', '--results', metavar='path', required=True, help="Directory to store the results")
     parser.add_argument('-d', '--depth', metavar='N', type=int, default=2, help="Recussion depth when looking for scans")
-    parser.add_argument('-m', '--misses', action='store_true', help="Will write a misses file containing all missed events")
+    parser.add_argument('-m', '--misses', action='store_true', help="Write a misses file containing all missed events")
+    parser.add_argument('-f', '--force', action="store_true", help="Force scans against all logs")
+    parser.add_argument('-R', '--recursive', action="store_true", help="Recursively search for yara rules")
+    parser.add_argument('-M', '--max', metavar='N', type=int, help="Set a maximum number of logs to scan")
     parsed = parser.parse_args()
     args = Args(parsed)
 
@@ -107,11 +131,16 @@ def main():
     rules.parseRules()
 
     if not rules.rules:
-        print("No rules loaded")
+        print("No rules loaded", file=sys.stderr)
         exit()
 
     logs = Logs(args)
-    logs.parseEventFiles()
+    logs.parseLogFiles()
+    print(logs.logs)
+    exit()
+    if not logs.logs:
+        print("No logs loaded", file=sys.stderr)
+        exit()
 
     for log in logs.logs:
         p = Process(target=scan, args=(log, rules.rules, args))
